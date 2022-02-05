55195637312a308e6517780cf959a0b405557136
Anitor <anitorabraham@gmail.com>
2022-02-05 16:07:32 +0100

    updates

--- house.php
+++ house.php
@@ -11,7 +11,7 @@
                                 <h3 class="rn-sub-badge"><span class="theme-gradient">Our Company's About
                                         Details.</span></h3>
                             </div> -->
-                            <h1 class="title display-one">MEET YOUR HEROES <br> BECOME A HERO</h1>
+                            <h2 class="title display-one">WIN A DREAM HOME FOR YOU <br> MAKE OTHERS DREAMS COME TRUE</h2>
                         </div>
                     </div>
                 </div>
@@ -22,10 +22,9 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="section-title text-center" data-sal="slide-up" data-sal-duration="700" data-sal-delay="100">
-                            <h4 class="subtitle "><span class="theme-gradient">Latests News</span></h4>
-                            <h2 class="title w-600 mb--20">Blogs On Travellings.</h2>
-                            <p class="description b1">We provide company and finance service for <br> startups and
-                                company business.</p>
+                            <!-- <h4 class="subtitle "><span class="theme-gradient">Latests News</span></h4> -->
+                            <h4>Dream experiences. Great causes.</h4>
+                            <p class="description b1">Omaze experiences raise funds for Charities Aid Foundation America (“CAF America”), a US-registered, 501(c)(3) public charity. 100% of donations go to CAF America, which will then grant the donations, minus experience fees and costs, to the nonprofit(s) identified on the experience page. For more info, see the “Fundraising Transparency” section at the bottom of each experience page.</p>
                         </div>
                     </div>
                 </div>
@@ -46,7 +45,40 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-12 mt--30" data-sal="slide-up" data-sal-duration="700">
+                        <div class="rn-card box-card-style-default">
+                            <div class="inner">
+                                <div class="thumbnail"><a class="image" href="blog-details.html"><img src="assets/images/blog/blog-01.jpg" alt="Blog Image"></a></div>
+                                <div class="content">
+                                    <ul class="rn-meta-list">
+                                        <li><a href="#">Irin Pervin</a></li>
+                                        <li class="separator">/</li>
+                                        <li>10 Dec 2021</li>
+                                    </ul>
+                                    <h4 class="title"><a href="blog-details.html">Best Corporate Tips You Will Read This
+                                            Year.</a></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 col-12 mt--30" data-sal="slide-up" data-sal-duration="700">
+                        <div class="rn-card box-card-style-default">
+                            <div class="inner">
+                                <div class="thumbnail"><a class="image" href="blog-details.html"><img src="assets/images/blog/blog-01.jpg" alt="Blog Image"></a></div>
+                                <div class="content">
+                                    <ul class="rn-meta-list">
+                                        <li><a href="#">Irin Pervin</a></li>
+                                        <li class="separator">/</li>
+                                        <li>10 Dec 2021</li>
+                                    </ul>
+                                    <h4 class="title"><a href="blog-details.html">Best Corporate Tips You Will Read This
+                                            Year.</a></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                
             </div>
         </div>
         
